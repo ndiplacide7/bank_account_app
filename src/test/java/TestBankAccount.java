fca4e9958fa4367ea41e434bc39e7d5564e2d488
Placide <placide.nduwayezu@rra.gov.rw>
2024-09-26 14:42:34 +0200

    Implementing Unit tests for deposit, withdraw, and transfer methods

--- src/test/java/TestBankAccount.java
+++ src/test/java/TestBankAccount.java
@@ -8,4 +8,27 @@ public class TestBankAccount {
         BankAccount account = new BankAccount(100);
         assertEquals(100, account.getBalance());
     }
+
+    @Test
+    public void shouldDepositCorrectAmount() {
+        BankAccount account = new BankAccount(100);
+        account.deposit(50);
+        assertEquals(150, account.getBalance());
+    }
+
+    @Test
+    public void shouldWithdrawCorrectAmount() {
+        BankAccount account = new BankAccount(100);
+        account.withdraw(50);
+        assertEquals(50, account.getBalance());
+    }
+
+    @Test
+    public void shouldTransferCorrectAmount() {
+        BankAccount account1 = new BankAccount(100);
+        BankAccount account2 = new BankAccount(100);
+        account1.transfer(account2, 50);
+        assertEquals(50, account1.getBalance());
+        assertEquals(150, account2.getBalance());
+    }
 }
